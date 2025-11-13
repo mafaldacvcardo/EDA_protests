99cec6390146f032c1b352dfafdbfa51cd8e0b16
mafaldacvcardo <mafaldacvc@gmail.com>
2025-11-13 07:38:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import utils.utils as utils
+
+def main():
+    print("FARPE DATA ANALYSIS")
+    
+    print("All analyses have been executed successfully")
+
+if __name__ == "__main__":
+    main()
